63b9d0b8bf539979904f9461d8be9f3adcd82769
Kelvys Moura <kelvysmoura2@gmail.com>
2024-10-11 08:35:01 -0300

    Backend auth

---
+++ javascript/auth/server.js
@@ -0,0 +1,103 @@
+/**
+ * Require necessary libraries
+ */
+const fs = require('fs')
+const bodyParser = require('body-parser')
+const jsonServer = require('json-server')
+// const jwt = require('jsonwebtoken')
+// const bcrypt = require('bcrypt')
+
+// JWT confing data
+const SECRET_KEY = '123456789'
+const expiresIn = '1h'
+
+// Create server
+const server = jsonServer.create()
+
+// Create router
+const router = jsonServer.router('./db.json')
+
+// Users database
+const userdb = JSON.parse(fs.readFileSync('./db.json', 'UTF-8'))
+
+// Default middlewares
+server.use(bodyParser.urlencoded({ extended: true }))
+server.use(bodyParser.json())
+
+// Create a token from a payload
+function createToken(payload) {
+  return jwt.sign(payload, SECRET_KEY, { expiresIn })
+}
+
+// Verify the token
+function verifyToken(token) {
+  return jwt.verify(
+    token,
+    SECRET_KEY,
+    (err, decode) => (decode !== undefined ? decode : err)
+  )
+}
+
+// Check if the user exists in database
+function isAuthenticated({ email, password }) {
+  return (
+    userdb.users.findIndex(
+      user =>
+        user.email === email && password === user.password
+    ) !== -1
+  )
+}
+
+/**
+ * Method: POST
+ * Endpoint: /auth/login
+ */
+server.post('/auth/login', (req, res) => {
+  const { email, password } = req.body
+  if (isAuthenticated({ email, password }) === false) {
+    const status = 401
+    const message = 'Incorrect email or password'
+    res.status(status).json({ status, message })
+    return
+  }
+  // const token = createToken({ email, password })
+  // res.status(200).json({ token })
+})
+
+server.get('/list', (req, res) => {
+    if(!req.headers.senha) {
+        const status = 401
+        const message = 'Bad authorization header'
+        return res.status(status).json({ status, message })
+    }
+    res.json(userdb.products)
+});
+
+/**
+ * Middleware: Check authorization
+ */
+// server.use(/^(?!\/auth).*$/, (req, res, next) => {
+//   if (
+//     req.headers.authorization === undefined ||
+//     req.headers.authorization.split(' ')[0] !== 'Bearer'
+//   ) {
+//     const status = 401
+//     const message = 'Bad authorization header'
+//     res.status(status).json({ status, message })
+//     return
+//   }
+//   try {
+//     verifyToken(req.headers.authorization.split(' ')[1])
+//     next()
+//   } catch (err) {
+//     const status = 401
+//     const message = 'Error: access_token is not valid'
+//     res.status(status).json({ status, message })
+//   }
+// })
+
+// Server mount
+server.use(router)
+server.listen(3000, () => {
+  console.log('Auth API server runing on port 3000 ...')
+})
