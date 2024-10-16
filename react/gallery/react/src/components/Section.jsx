67ce43ae8ee54e6d439c34536abf0d4072813a3b
Kelvys Moura <kelvysmoura2@gmail.com>
2024-10-16 09:51:01 -0300

    Component section

---
+++ react/gallery/react/src/components/Section.jsx
@@ -0,0 +1,19 @@
+export default function Section(props) {
+    let align = props.titleAlign ? props.titleAlign : 'left';
+    align = `text-${align}`;
+    return (
+        <>
+        <div className="flex">
+            <div className={align + ' grow'}>
+                <h2>{props.title}</h2>
+            </div>
+            {props.link && (
+                <div className="w-auto">
+                    <a href={props.link.href}>{props.link.text}</a>
+                </div>
+            )}
+        </div>
+        {props.children}
+        </>
+    )
+}
