34fc647065017a52a2a182d335c622d2f41b9df9
Kelvys Moura <kelvysmoura2@gmail.com>
2024-10-22 09:05:09 -0300

    Update store-ER.sql

--- database/store-ER.sql
+++ database/store-ER.sql
@@ -11,3 +11,45 @@ CREATE TABLE store.user (
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     update_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP 
 );
+
+CREATE TABLE store.product (
+	id INT PRIMARY KEY AUTO_INCREMENT,
+    name VARCHAR(255),
+    price DECIMAL(5, 2),
+    slug VARCHAR(255),
+    description TEXT,
+    price_with_discount DECIMAL(5, 2),
+    enabled BOOLEAN DEFAULT 0,
+    stock INT DEFAULT 0,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+);
+
+CREATE TABLE store.product_option (
+	id INT PRIMARY KEY AUTO_INCREMENT,
+    product_id INT,
+    title VARCHAR(20),
+    shape ENUM("square", "circle"),
+    radius INT DEFAULT 0,
+    type ENUM("text", "color"),
+    `values` VARCHAR(255)
+);
+
+CREATE TABLE store.product_image (
+	id INT PRIMARY KEY AUTO_INCREMENT,
+    product_id INT,
+    path VARCHAR(255)
+);
+
+CREATE TABLE store.category (
+	id INT PRIMARY KEY AUTO_INCREMENT,
+    name VARCHAR(45),
+    slug VARCHAR(255),
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+);
+
+CREATE TABLE store.product_category (
+	product_id INT,
+    category_id INT
+);
