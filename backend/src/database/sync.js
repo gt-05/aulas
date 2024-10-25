ba37045007cfc1041537129afa86d7200566d0b0
Kelvys Moura <kelvysmoura2@gmail.com>
2024-10-25 12:05:04 -0300

    Sequelize

---
+++ backend/src/database/sync.js
@@ -0,0 +1,12 @@
+const ProductModel = require('../models/ProductModel');
+const ProductImageModel = require('../models/ProductImageModel');
+
+async function execute() {
+    await ProductModel.sync();
+    await ProductImageModel.sync();
+}
+
+execute();
+
+
+
