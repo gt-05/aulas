797d50e06a63b0582256c0087c07f3f5f1faa71d
Kelvys Moura <kelvysmoura2@gmail.com>
2024-10-31 10:43:11 -0300

    Upload images

--- backend/src/controllers/Products/CreateController.js
+++ backend/src/controllers/Products/CreateController.js
@@ -1,8 +1,26 @@
+const fs = require('fs');
+const path = require("path");
 const ProductModel = require('../../models/ProductModel');
+const {saveByUrl} = require('../../services/product-images');
 
 module.exports = async (request, response) => {
-    let {body} = request;
-    let product = await ProductModel.create(body);
+    let {
+        name,
+        slug,
+        price
+    } = request.body;
+
+    let product = await ProductModel.create({
+        name, slug, price
+    });
+
+    let {images} = request.body;
+
+    for (let url of images) {
+        let {relativePath} = await saveByUrl({url, slug});
+        console.log(relativePath);
+    }
+
     response.status(201);
     return response.json(product);
 }
